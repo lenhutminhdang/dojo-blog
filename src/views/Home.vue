8011139152c1bb5e5884a139be89ca6287f20e0f
lenhutminhdang <ginoace2k1@gmail.com>
2023-09-23 00:35:17 +0700

    Test new branch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="home">
-    <h2>- - - Search Names - - -</h2>
-    <input type="text" v-model="search" placeholder="type to search..." />
-
-    <p v-for="name in matchingNames" :key="name">
-      {{ name }}
-    </p>
+    <h2>- - - Test New Branch - - -</h2>
   </div>
 </template>
 
@@ -13,31 +8,8 @@
 import { computed, ref } from "vue";
 export default {
   name: "Home",
-  setup() {
-    const search = ref("");
-    const names = ref(["mario", "yoshi", "luigi", "koopa", "marry", "peach"]);
-
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value));
-    });
-
-    return {
-      search,
-      matchingNames,
-    };
-  },
+  setup() {},
 };
 </script>
 
-<style>
-input {
-  box-sizing: border-box;
-  width: 12rem;
-  height: 2rem;
-  padding: 1rem;
-}
-
-input::placeholder {
-  font-style: italic;
-}
-</style>
+<style></style>
